e0480fd1db5fd4a280388d402aa86a2d0990301c
Calcifer077 <maheshnashier14@gmail.com>
2026-02-01 16:18:03 +0530

    Fixed error of CORS

--- Backend/app.js
+++ Backend/app.js
@@ -29,7 +29,7 @@ app.use(
 );
 
 // Explicit preflight for everything (Socket.IO polling loves this)
-app.options("*", cors());
+// app.options("*", cors());
 
 export const io = new Server(httpServer, {
   cors: {
